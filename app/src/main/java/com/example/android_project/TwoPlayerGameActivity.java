81e5e8220a81a692608e196eb5b1e66f9c0b6a75
GallaPriyanka <priyankagalla96@gmail.com>
2019-10-04 23:31:35 -0500

    added all project files

---
+++ app/src/main/java/com/example/android_project/TwoPlayerGameActivity.java
@@ -0,0 +1,14 @@
+package com.example.android_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TwoPlayerGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_player_game);
+    }
+}
