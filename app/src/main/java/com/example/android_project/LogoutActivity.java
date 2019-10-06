48733e71cfef8ab3b218a563cfb2582e863e1b21
GallaPriyanka <priyankagalla96@gmail.com>
2019-10-05 23:31:42 -0500

    created on click for logout button

--- app/src/main/java/com/example/android_project/LogoutActivity.java
+++ app/src/main/java/com/example/android_project/LogoutActivity.java
@@ -2,7 +2,9 @@ package com.example.android_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LogoutActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class LogoutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout);
     }
+
+    public void login_again_btn_onClick(View v) {
+        Intent login_again_intent = new Intent(this,LoginActivity.class);
+        startActivity(login_again_intent);
+    }
+
 }
