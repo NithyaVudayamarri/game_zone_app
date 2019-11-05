afc57392474d670e850602434e87867225e4841f
nagasaimanojgoppisetty <Manoj1028>
2019-11-04 23:23:28 -0600

    added stub to three player game activity

--- app/src/main/java/com/example/android_project/ThreePlayerGameActivity.java
+++ app/src/main/java/com/example/android_project/ThreePlayerGameActivity.java
@@ -3,6 +3,8 @@ package com.example.android_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ThreePlayerGameActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ThreePlayerGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three_player_game);
+    }
+
+    public void playBtnAction(View v) {
+        TextView progress_tv = findViewById(R.id.progressTV);
+        progress_tv.setText("Work In Progress. Please Come Back Later!");
     }
 }
