26f0a53a1847be60a894fa5481e8658b316db6fb
GallaPriyanka <priyankagalla96@gmail.com>
2019-11-04 21:13:13 -0600

    created an onClick event for the login button in login page

--- app/src/main/java/com/example/android_project/LoginActivity.java
+++ app/src/main/java/com/example/android_project/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.android_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void loginBTNAction(View view){
+        Intent home_intent = new Intent(this,HomepageActivity.class);
+        startActivity(home_intent);
+    }
+
 }
