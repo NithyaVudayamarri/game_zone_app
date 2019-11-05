17201d788bb7c0de966fa9999ffdf51fd5aad78c
GallaPriyanka <priyankagalla96@gmail.com>
2019-11-04 21:02:46 -0600

    removed the unwanted content in main/java/com/example/android_project/MainActivity.java

--- app/src/main/java/com/example/android_project/MainActivity.java
+++ app/src/main/java/com/example/android_project/MainActivity.java
@@ -13,10 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //declaring login and signup buttons
-        Button login_btn = (Button) findViewById(R.id.LoginActBtn);
-        Button signup_btn = (Button) findViewById(R.id.SignupActBtn);
-        Button logout_btn = (Button) findViewById(R.id.LogoutActBtn);
 
     }
 
