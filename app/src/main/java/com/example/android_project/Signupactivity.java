d0a980414813f4469d398b373b3a77f5d442f67d
GallaPriyanka <priyankagalla96@gmail.com>
2019-11-04 21:50:50 -0600

    saved the entered data in registration page

--- app/src/main/java/com/example/android_project/Signupactivity.java
+++ app/src/main/java/com/example/android_project/Signupactivity.java
@@ -2,7 +2,13 @@ package com.example.android_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Signupactivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class Signupactivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signupactivity);
+    }
+
+    public void signBTNAction(View v) {
+
+        SharedPreferences myData = getSharedPreferences("register details", Context.MODE_PRIVATE);
+        SharedPreferences.Editor ed = myData.edit();
+        EditText full_nameET = findViewById(R.id.full_nameET);
+        EditText email_addressET = findViewById(R.id.email_addressET);
+        EditText pwd = findViewById(R.id.passwordET);
+        EditText re_pwd = findViewById(R.id.re_enter_passwordET);
+        EditText mobile_numberET = findViewById(R.id.mobilenumberET);
+        EditText gender_ET = findViewById(R.id.genderET);
+        EditText country_ET = findViewById(R.id.countryET);
+        ed.putString("full name", full_nameET.getText().toString());
+        ed.putString("email address", email_addressET.getText().toString());
+        ed.putString("password", pwd.getText().toString());
+        ed.putString("re entered password", re_pwd.getText().toString());
+        ed.putString("mobile number", mobile_numberET.getText().toString());
+        ed.putString("gender", gender_ET.getText().toString());
+        ed.putString("country", country_ET.getText().toString());
+        ed.commit();
+        Toast.makeText(getApplicationContext(), "Registered successfully!", Toast.LENGTH_SHORT).show();
+
+
+
     }
 }
