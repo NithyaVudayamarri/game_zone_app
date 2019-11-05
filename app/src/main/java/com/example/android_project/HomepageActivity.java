5fe241532dcde75d33790394feb7d8033ad47ec5
nagasaimanojgoppisetty <Manoj1028>
2019-11-04 23:02:54 -0600

    added a link from the home page to the single player game page

--- app/src/main/java/com/example/android_project/HomepageActivity.java
+++ app/src/main/java/com/example/android_project/HomepageActivity.java
@@ -2,7 +2,9 @@ package com.example.android_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomepageActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class HomepageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
     }
+
+    public void singlePlayerBtnAction(View v) {
+        Intent single_player_intent = new Intent(this, SinglePlayerGameActivity.class);
+        startActivity(single_player_intent);
+    }
+
 }
