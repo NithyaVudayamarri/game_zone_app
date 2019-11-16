a7d8dc7b344d75b3c9aeb8ca559be25d61b5032d
GallaPriyanka <priyankagalla96@gmail.com>
2019-11-15 22:54:04 -0600

    included a function to validate data in registration activity

--- app/src/main/java/com/example/android_project/Registeractivity.java
+++ app/src/main/java/com/example/android_project/Registeractivity.java
@@ -1,5 +1,6 @@
 package com.example.android_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -12,24 +13,35 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 import org.w3c.dom.Text;
 
 public class Registeractivity extends AppCompatActivity {
+
+    EditText username_et;
+    EditText password_et;
+    EditText email_et;
+    Button register_btn;
+    TextView login_again_tv;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Button register_btn = findViewById(R.id.registerBTN);
-        TextView login_again_tv = findViewById(R.id.loginHereTV);
+        username_et = findViewById(R.id.usernameET);
+        password_et = findViewById(R.id.passwordET);
+        email_et = findViewById(R.id.emailET);
+        register_btn = findViewById(R.id.registerBTN);
+        login_again_tv = findViewById(R.id.loginHereTV);
 
-        register_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
 
         login_again_tv.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +51,22 @@ public class Registeractivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public Boolean validate() {
+        Boolean result = false;
+
+        String name = username_et.getText().toString();
+        String pwd = password_et.getText().toString();
+        String email = email_et.getText().toString();
+
+        if (name.isEmpty() || pwd.isEmpty() || email.isEmpty()) {
+            Toast.makeText(this, "Please enter the details." , Toast.LENGTH_SHORT).show();
+        }
+        else {
+            result = true;
+        }
+        return result;
     }
 
 }
