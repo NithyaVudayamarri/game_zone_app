01115af3014fe262015b604ed739da3c950a6bfa
nagasaimanojgoppisetty <Manoj1028>
2019-11-15 23:40:18 -0600

    connected LogoutActivity.java to mainactivity

--- app/src/main/java/com/example/android_project/LogoutActivity.java
+++ app/src/main/java/com/example/android_project/LogoutActivity.java
@@ -5,18 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LogoutActivity extends AppCompatActivity {
+    TextView login_again;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout);
-    }
 
-    public void login_again_btn_onClick(View v) {
-        Intent login_again_intent = new Intent(this,LoginActivity.class);
-        startActivity(login_again_intent);
+        login_again = findViewById(R.id.loginAgainTV);
+        login_again.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login_again_intent = new Intent(LogoutActivity.this, LoginActivity.class);
+                startActivity(login_again_intent);
+            }
+        });
     }
-
 }
